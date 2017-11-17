079d4940dc72f8950e337e58c27be5e8c5ec4552
Edmond Atto <edmond091@gmail.com>
2017-11-18 00:31:05 +0300

    refactor app

---
+++ src/components/BookCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const BookCard = props =>
+  <div className="book-card">
+    <h4>{ props.title }</h4>
+    <h5>{ props.author }</h5>
+    <p>{ props.description }</ p>
+    <p><strong>Publication date: </strong>{ props.publicationDate }</p>
+  </div>;
+
+BookCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  publicationDate: PropTypes.string.isRequired
+};
+
+export default BookCard;
