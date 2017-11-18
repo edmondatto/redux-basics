f0f57cbfe97655158e289ba46c57e05a817c388b
Edmond Atto <edmond091@gmail.com>
2017-11-18 10:10:44 +0300

    Add props and propTypes

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -13,7 +13,7 @@ BookCard.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  publicationDate: PropTypes.string.isRequired
+  publicationDate: PropTypes.string.isRequired,
 };
 
 export default BookCard;
