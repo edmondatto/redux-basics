6b0fd7d4e6d201cbdafdbb7b42a831575899d999
Edmond Atto <edmond091@gmail.com>
2017-11-18 10:09:33 +0300

    Add createBook event handler

---
+++ src/components/FormInput.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import {addBook} from "../actions/books";
+import PropTypes from 'prop-types';
+
+class FormInput extends Component {
+  createBook = event => {
+    event.preventDefault();
+    const title = this.title.value;
+    const author = this.author.value;
+    const description = this.description.value;
+    const publicationDate = this.publicationDate.value;
+    const data = {
+      title: title,
+      author: author,
+      description: description,
+      publicationDate: publicationDate,
+      id: 4
+    };
+    this.props.dispatch(addBook(data));
+
+  };
+
+  render() {
+
+    return (
+      <form onSubmit={this.createBook}>
+        Create a new Book Entry:
+        <input type="text" ref={(input) => this.title = input} placeholder="Enter Book title"/>
+        <input type="text" ref={(input) => this.author = input} placeholder="Enter Author name"/>
+        <input type="text" ref={(input) => this.description = input} placeholder="Enter Book description"/>
+        <input type="text" ref={(input) => this.publicationDate = input} placeholder="Enter publication date"/>
+        <input type="submit" value="SAVE"/>
+      </form>
+    )
+
+  }
+}
+
+FormInput.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default FormInput;
